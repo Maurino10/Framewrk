69685f488cbcbcc01ba38c7c1f328013396ffa0a
Maurino10 <126274853+Maurino10@users.noreply.github.com>
2023-07-08 01:10:24 +0300

    sprint5

---
+++ Sprint5/WEB-INF/src/FrontServlet.java
@@ -0,0 +1,63 @@
+package etu1809.framework.servlet;
+
+import java.io.*;
+import java.lang.reflect.Method;
+import javax.servlet.*; 
+import javax.servlet.http.*;
+import javax.servlet.annotation.MultipartConfig;
+import java.util.*;
+import etu1809.framework.*;
+
+public class FrontServlet extends HttpServlet {
+    HashMap<String, Mapping> mapping_url = null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+         try {
+            ServletContext ctx = getServletContext() ;
+            String path = ctx.getRealPath("/WEB-INF/classes/models");    
+            this.mapping_url = Utilitaire.getHashMap(path);            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void processRequest (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException { 
+        PrintWriter out = res.getWriter();
+        out.println("********* Url and Path *********");
+        out.println("Url: " + req.getRequestURL());
+        out.println("Contexte: " + req.getContextPath());
+        out.println("********* Url and Path *********");
+
+        out.println("********* Valeur dans HashMap *********");
+        Utilitaire.afficher_MappingUrls(this.mapping_url,out);
+        out.println("********* Valeur dans HashMap *********");
+        
+        String url = String.valueOf(req.getRequestURL());
+        String apres_contexte = Utilitaire.getUrl(url);
+        out.println("url ou valeur annotation: " + apres_contexte);
+
+        try {
+            if(apres_contexte != null) {
+                for (String key: mapping_url.keySet()) {
+                    if(apres_contexte.equals(key)) {
+                        out.println("Key  :"+key);
+                        out.println("Class  : "+mapping_url.get(key).getClassName());                       
+                        out.println("Method  : "+mapping_url.get(key).getMethod());                       
+                    } 
+                }        
+            }                
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException { 
+        processRequest(req, res);
+    }
+
+    protected void doPost (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException { 
+        processRequest(req, res);
+    }
+}
